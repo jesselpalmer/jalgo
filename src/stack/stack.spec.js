7cb3c48c6684345ae66e4253bec4c69aac395e9e
Jesse Palmer <jesselpalmer@gmail.com>
2017-06-17 22:45:27 -0700

    test: add tests

--- src/stack/stack.spec.js
+++ src/stack/stack.spec.js
@@ -3,12 +3,14 @@
 import { expect } from 'chai'
 import Stack from './stack'
 
-describe('SuperStack class', () => {
+describe('Stack class', () => {
   describe('clear() method', () => {
     it('should clear an array with elements', () => {
       const stack = new Stack()
       stack.push('stanford')
       stack.push('cmu')
+      stack.push('berkeley')
+      stack.push('mit')
       stack.clear()
       expect(stack.size()).to.be.equal(0)
     })
@@ -49,6 +51,34 @@ describe('SuperStack class', () => {
     })
   })
 
+  describe('pop() method', () => {
+    it('should return the first element', () => {
+      const stack = new Stack()
+      stack.push('babbage')
+      stack.push('hopper')
+      stack.push('knuth')
+      expect(stack.pop()).to.be.equal('babbage')
+    })
+
+    it('should remove first element', () => {
+      const stack = new Stack()
+      stack.push('babbage')
+      stack.push('hopper')
+      stack.pop()
+      stack.push('knuth')
+      expect(stack.peek()).to.be.equal('hopper')
+    })
+
+    it('should had the correct size', () => {
+      const stack = new Stack()
+      stack.push('babbage')
+      stack.push('hopper')
+      stack.pop()
+      stack.push('knuth')
+      expect(stack.size()).to.be.equal(2)
+    })
+  })
+
   describe('size() method', () => {
     it('should return the correct size', () => {
       const stack = new Stack()
@@ -61,6 +91,20 @@ describe('SuperStack class', () => {
     it('should return [] when stack is empty', () => {
       const stack = new Stack()
       expect(stack.toString()).to.be.equal('[]')
+    })
+
+    it('should return correct string when there one element', () => {
+      const stack = new Stack()
+      stack.push(1)
+      expect(stack.toString()).to.be.equal('[ 1 ]')
+    })
+
+    it('should return correct string when there are multiple elements', () => {
+      const stack = new Stack()
+      stack.push(1)
+      stack.push(2)
+      stack.push(3)
+      expect(stack.toString()).to.be.equal('[ 1, 2, 3 ]')
     })
   })
 })
