84599d243805fa18a41e06022253c50f508c5ecb
Jesse Palmer <jesselpalmer@gmail.com>
2019-03-10 14:27:15 -0700

    refactor: simplify for readability

--- src/algorithms/linear-search/linear-search.js
+++ src/algorithms/linear-search/linear-search.js
@@ -1,13 +1,5 @@
 export const linearSearch = (list, item) => {
-  if (list === null || list === undefined) {
-    return -1
-  }
-  
-  if (list[0] === item) {
-    return 0
-  }
-  
-  for (let i = 1; i < list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     const currentItem = list[i]
     
     if (currentItem === item) {
--- src/algorithms/linear-search/linear-search.spec.js
+++ src/algorithms/linear-search/linear-search.spec.js
@@ -13,19 +13,4 @@ describe('linearSearch tests', () => {
     const numList = [-121, 0, 1, 2, 3, 4, 5, 1000, 9999]
     expect(linearSearch(numList, 17)).to.be.equal(-1)
   })
-
-  it('should return -1 an element if array is empty', () => {
-    const numList = []
-    expect(linearSearch(numList, 97)).to.be.equal(-1)
-  })
-
-  it('should return -1 an element if array is undefined', () => {
-    const numList = undefined
-    expect(linearSearch(numList, 52)).to.be.equal(-1)
-  })
-
-  it('should return -1 an element if array is null', () => {
-    const numList = null
-    expect(linearSearch(numList, 11)).to.be.equal(-1)
-  })
 })
