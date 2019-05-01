18125a1110f350ac53204c8239e8e5f6bbc5482f
Jesse Palmer <jesselpalmer@gmail.com>
2019-05-01 13:09:42 -0700

    refactor: allows for different data types

--- src/algorithms/binary-search/binary-search.js
+++ src/algorithms/binary-search/binary-search.js
@@ -6,34 +6,34 @@
  * Average performance:           O(log n)
  * Worst-case space complexity:   O(1)
  * 
- * @param {number} numList - a sorted list of numbers
- * @param {number} num - the number that you are looking for
+ * @param {array<values>} valuesArray - a sorted array of values
+ * @param {any} value - the value that you are looking for
  * 
- * @returns {number} - the index if the number is found or null if the number is not found
+ * @returns {any} - the index if the value is found or -1 if the value is not found
  */
-export const binarySearch = (numList, num) => {
-  const numLength = numList.length
+export const binarySearch = (valuesArray, value) => {
+  const valuesLength = valuesArray.length
   let low = 0
-  let high = numLength - 1
+  let high = valuesLength - 1
   let guess = null
   let mid = 0
   
   while (low <= high) {
     mid = Math.round((low + high) / 2)
-    guess = numList[mid]
+    guess = valuesArray[mid]
     
-    if (guess === num) {
+    if (guess === value) {
       return mid
     }
     
-    if (guess > num) {
+    if (guess > value) {
       high = mid - 1
     }
     
-    if (guess < num) {
+    if (guess < value) {
       low = mid + 1
     }
   }
   
-  return null
+  return -1
 }
--- src/algorithms/binary-search/binary-search.spec.js
+++ src/algorithms/binary-search/binary-search.spec.js
@@ -5,17 +5,22 @@ import { binarySearch } from './binary-search'
 
 describe('Binary Search tests', () => {
   it('should find an element if it exists', () => {
-    const numList = [-121, 0, 1, 2, 3, 4, 5, 1000, 9999]
-    expect(binarySearch(numList, 3)).to.be.equal(4)
+    const testArray = [-121, 0, 1, 2, 3, 4, 5, 1000, 9999]
+    expect(binarySearch(testArray, 3)).to.be.equal(4)
   })
 
-  it('should return null an element if it exists', () => {
-    const numList = [-121, 0, 1, 2, 3, 4, 5, 1000, 9999]
-    expect(binarySearch(numList, 25)).to.be.equal(null)
+  it('should return index if an element exists', () => {
+    const testArray = ['a','b','c','d']
+    expect(binarySearch(testArray, 'b')).to.be.equal(1)
   })
 
-  it('should return null an element if array is empty', () => {
-    const numList = []
-    expect(binarySearch(numList, 25)).to.be.equal(null)
+  it('should return index if an array of different data types exists', () => {
+    const testArray = [-42324, "b", null, undefined]
+    expect(binarySearch(testArray, null)).to.be.equal(2)
+  })
+
+  it('should return -1 if an array is empty', () => {
+    const testArray = []
+    expect(binarySearch(testArray, 25)).to.be.equal(-1)
   })
 })
