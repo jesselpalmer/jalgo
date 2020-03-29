6e2da2a473ee8dff79cffc103e026e149665d9e4
Jesse Palmer <jesselpalmer@gmail.com>
2020-03-29 16:32:07 -0700

    feat: add hashset

---
+++ src/data-structures/hashset/hashset.js
@@ -0,0 +1,19 @@
+class HashSet {
+  values = {}
+
+  add(val) {
+    this.values[val] = true
+  }
+
+  has(val) {
+    return this.values[val] === true
+  }
+
+  remove(val) {
+    delete this.values[val]
+  }
+
+  getValues() {
+    return Object.keys(this.values)
+  }
+}
