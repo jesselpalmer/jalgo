16e2e0b21eb75f17c502c5063d328665471be9a7
Jesse Palmer <jesselpalmer@gmail.com>
2020-04-04 09:52:41 -0700

    test: add hashset tests

--- src/data-structures/hashset/hashset.spec.js
+++ src/data-structures/hashset/hashset.spec.js
@@ -4,10 +4,16 @@ import { expect } from 'chai'
 import HashSet from './hashset'
 
 describe('HashSet tests', () => {
-  describe('smoke test', () => {
-    it('should successfully create instance', () => {
+  describe('has() tests', () => {
+    it('should be true if hashset contains value', () => {
       const hashSet = new HashSet()
-      expect(hashSet).not.equal(null)
+      hashSet.add(5)
+      expect(hashSet.has(5)).equal(true)
+    })
+
+    it('should be false if hashset does not contain value', () => {
+      const hashSet = new HashSet()
+      expect(hashSet.has(5)).equal(false)
     })
   })
 })
